7e34b785eaa7573d4af30ca4637be94dc2b30217
magdalenarajic <57672483+magdalenarajic@users.noreply.github.com>
2021-04-13 12:07:48 +0200

    Add files via upload

--- DZ002_002/index1.php
+++ DZ002_002/index1.php
@@ -13,53 +13,83 @@ th {text-align: left;}
 </style>
 <?php
 
-$connect = mysqli_connect("localhost", "root", "", "employees");
-$output = '';
-if(isset($_POST["query"]))
-{
-	$search = mysqli_real_escape_string($connect, $_POST["query"]);
-	$query = "
-	SELECT * FROM employees 
-	WHERE first_name LIKE '%".$search."%'
-	";
-}
-else
-{
-	$query = "
-	SELECT * FROM employees LIMIT 1000";
-}
-$result = mysqli_query($connect, $query);
-if(mysqli_num_rows($result) > 0)
-{
-	$output .= '<div>
-					<table>
-						<tr>
-							<th>Ime </th>
-							<th>Prezime</th>
-              <th>Datum rođenja</th>
-							<th>Spol</th>
-							<th>Datum zaposlenja</th>
-							
-						</tr>';
-	while($row = mysqli_fetch_array($result))
-	{
-		$output .= '
-			<tr>
-				<td>'.$row[2].'</td>
-				<td>'.$row[3].'</td>
-				<td>'.$row[1].'</td>
-				<td>'.$row[4].'</td>
-        <td>'.$row[5].'</td>
-				
-			</tr>
-		';
-	}
-	echo $output;
-}
-else
-{
-	echo 'Data Not Found';
-}
+$s = $_REQUEST["s"];
+$hint = "";
 
+$s = strtolower($s);
+$len=strlen($s);
+    /*
+	foreach($s as $name) {
+        if (stristr($s, substr($name, 0, $len))) {
+            if ($hint === "") {
+                $hint = $name;
+            } else {
+                $hint .= ", $name";
+            }
+        }
+    
+	}
+	*/
+	
+	
+
+/**********************************************************/
+$db = mysqli_connect("localhost","root","");
+
+if($db) {
+
+$result = mysqli_select_db($db, "employees") or die("Došlo je do problema prilikom odabira baze...");
+if ($s == "") {
+	$sql="SELECT * FROM employees LIMIT 1000";
+}
+else{
+$sql="SELECT * FROM employees where first_name LIKE '$s%'";
+}
+//echo $sql;
+$result2 = mysqli_query($db, $sql) or die("Došlo je do problema prilikom izvrsavanja upita...");
+$n=mysqli_num_rows($result2);
+
+if ($n > 0){
+	
+		echo "<table class=table>
+			<thead>
+			 <th>ID</th>
+			 <th>Ime</th>
+			 <th>Prezime</th>
+			 <th>Spol</th>
+			 <th>Datum zaposlenja</th>
+			</thead>";
+	while ($myrow=mysqli_fetch_row($result2)){
+		 
+		echo " <tbody>
+			  <tr>
+			    <td data-label='ID'>$myrow[0]</td>
+			    <td data-label='Ime'>$myrow[2]</td>
+			    <td data-label='Prezime'>$myrow[3]</td>
+			    <td data-label='Spol'>$myrow[4]</td>
+		            <td data-label='Datum zaposlenja'>$myrow[5]</td>
+			 </tr>
+		       </tbody>";
+			
+			//echo "<div name=\"result2\" id=\"".$myrow[0]."\">" .$myrow[2].", ".$myrow[3]."</div>";
+			
+
+		}
+		 echo "</table>";
+	}
+else {
+echo "<span>Nema rezultata za prikaz<br></span>";
+}	
+}
+else {
+echo "<br>Nije proslo spajanje<br>";
+}
+/**********************************************************/
+	
+
+
+// Output "no suggestion" if no hint was found or output correct values 
+//echo $hint === "" ? "no suggestion" : $hint;
 
 ?>
+
