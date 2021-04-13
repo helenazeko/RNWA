39073b4d614df538048664d0c0143cd786def552
magdalenarajic <57672483+magdalenarajic@users.noreply.github.com>
2021-04-13 12:07:20 +0200

    Add files via upload

--- DZ002_001/index1.php
+++ DZ002_001/index1.php
@@ -12,16 +12,14 @@ table, td, th {
 th {text-align: left;}
 </style>
 <?php
-// get the q parameter from URL
+
 $s = $_REQUEST["s"];
 $hint = "";
 
-// lookup all hints from array if $q is different from "" 
-if ($s !== "") {
-    $s = strtolower($s);
-    $len=strlen($s);
+$s = strtolower($s);
+$len=strlen($s);
     /*
-	foreach($a as $name) {
+	foreach($s as $name) {
         if (stristr($s, substr($name, 0, $len))) {
             if ($hint === "") {
                 $hint = $name;
@@ -29,9 +27,10 @@ if ($s !== "") {
                 $hint .= ", $name";
             }
         }
-    }
-	
+    
+	}
 	*/
+	
 	
 
 /**********************************************************/
@@ -39,40 +38,47 @@ $db = mysqli_connect("localhost","root","");
 
 if($db) {
 
-$result = mysqli_select_db($db, "employees") or die("Doslo je do problema prilikom odabira baze...");
-$sql="SELECT * FROM employees where first_name LIKE '$s%' ";
+$result = mysqli_select_db($db, "employees") or die("Došlo je do problema prilikom odabira baze...");
+if ($s == "") {
+	$sql="SELECT * FROM employees LIMIT 1000";
+}
+else{
+$sql="SELECT * FROM employees where first_name LIKE '$s%'";
+}
 //echo $sql;
-$result2 = mysqli_query($db, $sql) or die("Doslo je do problema prilikom izvrsavanja upita...");
+$result2 = mysqli_query($db, $sql) or die("Došlo je do problema prilikom izvrsavanja upita...");
 $n=mysqli_num_rows($result2);
 
-
-
 if ($n > 0){
-  echo "<table>
-<tr>
-<th>Ime</th>
-<th>Prezime</th>
-<th>Datum rođenja</th>
-<th>Spol</th>
-<th>Datum zaposlenja</th>
-</tr>";
-
+	
+		echo "<table class=table>
+			<thead>
+			 <th>ID</th>
+			 <th>Ime</th>
+			 <th>Prezime</th>
+			 <th>Spol</th>
+			 <th>Datum zaposlenja</th>
+			</thead>";
 	while ($myrow=mysqli_fetch_row($result2)){
-    echo "<tr>";
-    echo "<td>" . $myrow[2] . "</td>";
-    echo "<td>" . $myrow[3] . "</td>";
-    echo "<td>" . $myrow[1] . "</td>";
-    echo "<td>" . $myrow[4] . "</td>";
-    echo "<td>" . $myrow[5] . "</td>";
-    echo "</tr>";
-   // echo $myrow[0].",".$myrow[1].",".$myrow[2];
-			//$hint .= "<div name=\"result\" id=\"".$myrow[1]."\">".$myrow[2]." ".$myrow[3].",</div>";
+		 
+		echo " <tbody>
+			  <tr>
+			    <td data-label='ID'>$myrow[0]</td>
+			    <td data-label='Ime'>$myrow[2]</td>
+			    <td data-label='Prezime'>$myrow[3]</td>
+			    <td data-label='Spol'>$myrow[4]</td>
+		            <td data-label='Datum zaposlenja'>$myrow[5]</td>
+			 </tr>
+		       </tbody>";
+			
+			//echo "<div name=\"result2\" id=\"".$myrow[0]."\">" .$myrow[2].", ".$myrow[3]."</div>";
+			
 
 		}
-    echo "</table>";
+		 echo "</table>";
 	}
 else {
-//echo "No patern rows returned<br>";
+echo "<span>Nema rezultata za prikaz<br></span>";
 }	
 }
 else {
@@ -80,9 +86,10 @@ echo "<br>Nije proslo spajanje<br>";
 }
 /**********************************************************/
 	
-}
+
 
 // Output "no suggestion" if no hint was found or output correct values 
-echo $hint === "" ? "no suggestion" : $hint;
+//echo $hint === "" ? "no suggestion" : $hint;
 
 ?>
+
